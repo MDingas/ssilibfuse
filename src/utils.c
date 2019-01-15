c1450d58d67912fc926897ccf537b254d72fe035
MDingas <a79089@alunos.uminho.pt>
2019-01-15 01:07:46 +0000

    todas as funcoes deixam de retornar buffers, mas sim transformam os recebidos como parametros

--- src/utils.c
+++ src/utils.c
@@ -7,19 +7,18 @@
 #include <string.h>
 #include "utils.h"
 
-void generate_rand_alphanumeric_string(int size, char* str) {
+void generate_rand_alphanumeric_string(int size, char* buffer) {
         char* alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
         // Generate seed
         srand(time(NULL));
         for (int i = 0; i < size; i++) {
-                str[i] = alphabet[(rand() % ((strlen(alphabet)) - 1)) + 0];
+                buffer[i] = alphabet[(rand() % ((strlen(alphabet)) - 1)) + 0];
         }
-        str[size] = '\0';
+        buffer[size] = '\0';
 }
 
-char* get_user_email(char* credentials){
-        char buffer[BUFFER_SIZE];
+char* get_user_email(char* credentials,char* buffer){
         char* username = getenv("USER");
 
         snprintf(buffer, BUFFER_SIZE,"gawk -f get_email.awk username=%s %s", username, credentials);
@@ -32,7 +31,5 @@ char* get_user_email(char* credentials){
 
         //Email not found
         if(n <= 0) return NULL;
-
-        return strdup(buffer);
 }
 
--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,5 @@
 #define BUFFER_SIZE 100
 
 void generate_rand_alphanumeric_string(int size, char* str);
+char* get_user_email(char* credentials,char* buffer);
+
